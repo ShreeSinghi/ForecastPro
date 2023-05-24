872b0bda92e44959cd3687c1ff8a22ae49bef4f1
Shree Singhi <singhishree@gmail.com>
2023-05-24 14:50:21 +0530

    fixes bug

--- static/script.js
+++ static/script.js
@@ -1,87 +1,91 @@
 try{
 
-// Function to fetch test and prediction data from Flask server
-function fetchData() {
-  fetch('/data')
-    .then(response => response.json())
-    .then(data => {
-      // Extract test and prediction data from the response
-      const { test, predictions } = data;
-      addChart(test, predictions)
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
-
-
-function addChart(test, predictions){
-  // Chart data
-  var data = {
-    labels : [...Array(test.length).keys()],
-    datasets : [
-        {
-          data : test,
-          label : "Test",
-          borderColor : "#3cba9f",
-        },
-        {
-          data : predictions,
-          label : "Predictions",
-          borderColor : "#FF5D53",
-        }
-      ]
-  };
-
-  // Chart options
-  var options = {
-    responsive: true,
-    // scales: {
-    //   y: {
-    //     beginAtZero: true
-    //   }
-    // }
-    plugins: {
-      zoom: {
-        pan: {
-          enabled: true
-        },
-        zoom: {
-          wheel: {
-            enabled: true,
+  // Function to fetch test and prediction data from Flask server
+  function fetchData() {
+    fetch('/data')
+      .then(response => response.json())
+      .then(data => {
+        // Extract test and prediction data from the response
+        var { test, predictions } = data;
+  
+        test = test.map(element => element[1])
+        predictions = predictions.map(element => element[1])
+  
+        addChart(test, predictions)
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  
+  
+  function addChart(test, predictions){
+    // Chart data
+    var data = {
+      labels : [...Array(test.length).keys()],
+      datasets : [
+          {
+            data : test,
+            label : "Test",
+            borderColor : "#3cba9f",
           },
-          pinch: {
+          {
+            data : predictions,
+            label : "Predictions",
+            borderColor : "#FF5D53",
+          }
+        ]
+    };
+  
+    // Chart options
+    var options = {
+      responsive: true,
+      // scales: {
+      //   y: {
+      //     beginAtZero: true
+      //   }
+      // }
+      plugins: {
+        zoom: {
+          pan: {
             enabled: true
           },
-          mode: 'xy',
-        },
-        limits : {
-          y: {
-            max: 'original',
-            min: 'original'
+          zoom: {
+            wheel: {
+              enabled: true,
+            },
+            pinch: {
+              enabled: true
+            },
+            mode: 'xy',
+          },
+          limits : {
+            y: {
+              max: 'original',
+              min: 'original'
+            }
           }
         }
       }
-    }
-
-  };
-
-  // Create the chart
-  new Chart(document.getElementById("chart"), {
-    type : 'line',
-    data : data,
-    options : options
-  });
-
-}
-
-// Call the fetchData function when the page loads
-window.addEventListener('load', fetchData);
-
-// Get the canvas element
-var ctx = document.getElementById('chart').getContext('2d');
-
-} catch(err) {
-  console.error(err)
   
-}
+    };
+  
+    // Create the chart
+    new Chart(document.getElementById("chart"), {
+      type : 'line',
+      data : data,
+      options : options
+    });
+  
+  }
+  
+  // Call the fetchData function when the page loads
+  window.addEventListener('load', fetchData);
+  
+  // Get the canvas element
+  var ctx = document.getElementById('chart').getContext('2d');
+  
+  } catch(err) {
+    console.error(err)
+    
+  }
